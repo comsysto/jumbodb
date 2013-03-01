ff081701ec88eff9020edcb4362f7bcabde1747c
Carsten Hufe <carsten.hufe@comsysto.com>
2013-03-01 11:00:26 +0100

    upgraded to play 2.1

--- database/project/Build.scala
+++ database/project/Build.scala
@@ -1,6 +1,6 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
@@ -8,17 +8,17 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
+      "org.webjars" % "jquery" % "1.9.1",
+      "org.webjars" % "jquery-ui" % "1.9.2",
+      "org.webjars" % "bootstrap" % "2.3.0",
+      "org.webjars" % "angularjs" % "1.1.3",
       "commons-io" %  "commons-io"  % "2.4",
-      "com.jquery" %  "jquery"  % "1.7.2-1",
-      "com.jqueryui" %  "jquery.ui"  % "1.8.20",
-      "org.angularjs" %  "angular"  % "1.0.1-1",
       "org.xerial.snappy" %  "snappy-java"  % "1.0.4.1",
-      "com.github.twitter" %  "bootstrap"  % "2.1.0",
-      "net.minidev" %  "json-smart"  % "1.1.1"
+      "net.minidev" %  "json-smart"  % "1.1.1",
+      "com.google.guava" %  "guava"  % "13.0"
+  )
+
+    val main = play.Project(appName, appVersion, appDependencies).settings(
 
     )
-
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-    )
-
 }

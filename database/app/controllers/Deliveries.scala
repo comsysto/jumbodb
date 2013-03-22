8ccbd8c03059ab27f15c4a7af8ac6ba454a55e13
Carsten Hufe <carsten.hufe@comsysto.com>
2013-03-22 10:38:19 +0100

    old code removed

--- database/app/controllers/Deliveries.scala
+++ database/app/controllers/Deliveries.scala
@@ -70,53 +70,6 @@ object Deliveries extends Controller {
     } sortWith(_.date > _.date)
   }
 
-  //////////////
-
-//  def chunkedDeliveries = {
-//    val collectionFolders = dataPath.listFiles.filter(_.isDirectory).filter(!_.getName.startsWith("."))
-//    collectionFolders map { f =>
-//        val collectionName = f.getName
-//        JumboCollection(
-//          collapseId = "col" + f.getName.hashCode,
-//          name = collectionName,
-//          chunks = chunks(f, collectionName)
-//        )
-//    } sortBy(_.name)
-//  }
-//
-//  def chunks(collectionFolder: File, collectionName: String) = {
-//    val chunkFolders = collectionFolder.listFiles.filter(_.isDirectory)
-//    chunkFolders map { f =>
-//      val activeProps = ConfigFactory.parseFile(new File(f.getAbsolutePath + "/active.properties"))
-//      val chunkKey = f.getName
-//        DeliveryChunk(
-//          key = chunkKey,
-//          versions = versions(f, collectionName, chunkKey, activeProps.getString("deliveryVersion"))
-//        )
-//    }
-//  }
-//
-//  def versions(chunkFolder: File, collectionName: String, chunkKey: String, activeVersion: String) = {
-//    val versionFolders = chunkFolder.listFiles.filter(_.isDirectory)
-//    versionFolders map {  f =>
-//      val deliveryInfo = ConfigFactory.parseFile(new File(f.getAbsolutePath + "/delivery.properties"))
-//      val version = f.getName
-//      val compressedSize = FileUtils.sizeOfDirectory(f)
-//      val uncompressedSize = calculateUncompressed(f)
-//      val indexFolder = new File(indexPath.getAbsolutePath + "/" + collectionName + "/" + chunkKey + "/" + version)
-//      val indexSize = if(indexFolder.exists()) FileUtils.sizeOfDirectory(indexFolder) else 0
-//
-//        DeliveryVersion(
-//          version = version,
-//          info = deliveryInfo.getString("info"),
-//          date = deliveryInfo.getString("date"),
-//          compressedSize = compressedSize,
-//          uncompressedSize = uncompressedSize,
-//          indexSize = indexSize,
-//          active = (activeVersion == version)
-//        )
-//    } sortWith(_.date > _.date)
-//  }
 
   def calculateUncompressed(versionsFolder: File) = {
     val metaFiles = versionsFolder.listFiles.filter(!_.isDirectory).filter(_.getName.endsWith(".chunks.snappy"))

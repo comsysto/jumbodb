3ad2f494afa67dfb5ef1f9ee9ea87ff3ee440c80
Carsten Hufe <carsten.hufe@comsysto.com>
2013-04-04 20:57:43 +0200

    buttons implementet

--- database/src/main/webapp/js/controllers.js
+++ database/src/main/webapp/js/controllers.js
@@ -6,13 +6,90 @@ function OverviewCtrl($scope, $http) {
 }
 
 function CollectionsListCtrl($scope, $http) {
-    $http.get('/jumbodb/rest/collections').success(function(data) {
-        $scope.collections = data;
-    });
+    fetchData();
+    $scope.msg = {}
+
+    $scope.activateChunkedVersionInCollection = function(chunkDeliveryKey, version, collection) {
+        $http.put('/jumbodb/rest/version/' + chunkDeliveryKey + '/' + version + '/' + collection).success(function(data) {
+            fetchData();
+            buildMessage(data)
+        });
+    }
+
+    $scope.deleteChunkedVersionInCollection = function(chunkDeliveryKey, version, collection) {
+        $http.delete('/jumbodb/rest/version/' + chunkDeliveryKey + '/' + version + '/' + collection).success(function(data) {
+            fetchData();
+            buildMessage(data);
+        });
+    }
+
+    $scope.deleteCompleteCollection = function(collection) {
+        $http.delete('/jumbodb/rest/collection/' + collection).success(function(data) {
+            fetchData();
+            buildMessage(data)
+        });
+    }
+
+    function buildMessage(data) {
+        var msg = {};
+        msg.error = (data.type == 'delete')
+        msg.success = (data.type == 'activate')
+        msg.message = data.message;
+        $scope.msg = msg;
+    }
+
+    function fetchData() {
+        $http.get('/jumbodb/rest/collections').success(function(data) {
+            $scope.collections = data;
+        });
+    }
 }
 
 function DeliveriesListCtrl($scope, $http) {
-    $http.get('/jumbodb/rest/deliveries').success(function(data) {
-        $scope.deliveries = data;
-    });
+    fetchData();
+    $scope.msg = {}
+
+    // CARSTEN reuse ?
+
+    $scope.activateChunkedVersionForAllCollections = function(chunkDeliveryKey, version) {
+        $http.put('/jumbodb/rest/version/' + chunkDeliveryKey + '/' + version).success(function(data) {
+            fetchData();
+            buildMessage(data)
+        });
+    }
+
+    $scope.activateChunkedVersionInCollection = function(chunkDeliveryKey, version, collection) {
+        $http.put('/jumbodb/rest/version/' + chunkDeliveryKey + '/' + version + '/' + collection).success(function(data) {
+            fetchData();
+            buildMessage(data)
+        });
+    }
+
+    $scope.deleteChunkedVersionForAllCollections = function(chunkDeliveryKey, version) {
+        $http.delete('/jumbodb/rest/version/' + chunkDeliveryKey + '/' + version).success(function(data) {
+            fetchData();
+            buildMessage(data);
+        });
+    }
+
+    $scope.deleteChunkedVersionInCollection = function(chunkDeliveryKey, version, collection) {
+        $http.delete('/jumbodb/rest/version/' + chunkDeliveryKey + '/' + version + '/' + collection).success(function(data) {
+            fetchData();
+            buildMessage(data);
+        });
+    }
+
+    function buildMessage(data) {
+        var msg = {};
+        msg.error = (data.type == 'delete')
+        msg.success = (data.type == 'activate')
+        msg.message = data.message;
+        $scope.msg = msg;
+    }
+
+    function fetchData() {
+        $http.get('/jumbodb/rest/deliveries').success(function(data) {
+            $scope.deliveries = data;
+        });
+    }
 }

46ac24fc897d41102f93e8cacc9be3edc470da5e
Ulf Gitschthaler <ulf.gitschthaler@comsysto.com>
2013-04-18 17:51:28 +0200

    Add base idea for index resolvers and managers

---
+++ database/src/main/java/org/jumbodb/database/index/resolver/IndexResolver.java
@@ -0,0 +1,16 @@
+package org.jumbodb.database.index.resolver;
+
+/**
+ * @author ugitsch
+ */
+public class IndexResolver {
+
+
+    /**
+     * Type used to represent an index value, most likely primitive types (as objects) and Strings
+     *
+     * @return index class type (e.g. int)
+     */
+    //Class<T> indexType();
+
+}
---
+++ database/src/main/java/org/jumbodb/database/index/resolver/IndexResolverManager.java
@@ -0,0 +1,23 @@
+package org.jumbodb.database.index.resolver;
+
+/**
+ * @author ugitsch
+ */
+public class IndexResolverManager {
+
+
+
+
+    public IndexResolver getResolver(String strategy){
+        return null;
+    }
+
+    public IndexResolver getResolver(String collection, String chunkName, String indexname){
+        return null;
+    }
+
+    public IndexResolver getResolverName(String collection, String chunkName, String indexname){
+        return null;
+    }
+
+}

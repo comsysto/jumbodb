9e3b6050704e41f4e88d5330cf52f098de6c4fbf
Carsten Hufe <carsten.hufe@comsysto.com>
2013-04-26 10:14:41 +0200

    one bithash operation removed

--- commons/query/src/main/java/org/jumbodb/common/query/QueryOperation.java
+++ commons/query/src/main/java/org/jumbodb/common/query/QueryOperation.java
@@ -1,5 +1,5 @@
 package org.jumbodb.common.query;
 
 public enum QueryOperation {
-    EQ, NE, LT, GT, BETWEEN, GEO_BOUNDARY_BOX, GEO_WITHIN_RANGE_METER, GEO_GEOHASH
+    EQ, NE, LT, GT, BETWEEN, GEO_BOUNDARY_BOX, GEO_WITHIN_RANGE_METER
 }

e425cbb1241f2d647379d5d312fc16c5fb82ab10
Carsten Hufe <carsten.hufe@comsysto.com>
2013-06-28 17:35:18 +0200

    specs

---
+++ database/src/test/groovy/org/jumbodb/database/rest/RestControllerSpec.groovy
@@ -0,0 +1,12 @@
+package org.jumbodb.database.rest
+
+import spock.lang.Specification
+
+
+/**
+ * @author Carsten Hufe
+ */
+class RestControllerSpec extends Specification {
+    // TODO
+
+}
--- database/src/test/groovy/org/jumbodb/database/service/query/data/snappy/JsonSnappyRetrieveDataSetsTaskSpec.groovy
+++ database/src/test/groovy/org/jumbodb/database/service/query/data/snappy/JsonSnappyRetrieveDataSetsTaskSpec.groovy
@@ -3,6 +3,50 @@ package org.jumbodb.database.service.query.data.snappy
 /**
  * @author Carsten Hufe
  */
-class JsonSnappyRetrieveDataSetsTaskSpec {
-    // TODO
+class JsonSnappyRetrieveDataSetsTaskSpec extends spock.lang.Specification {
+    def task = JsonSnappyRetrieveDataSetsTask()
+
+    def "integrated test indexed search"() {
+        // TODO
+    }
+
+    def "integrated test scanned search"() {
+        // TODO
+    }
+
+    def "getResultBuffer calculate buffer size for data read"() {
+       // TODO
+    }
+
+    def "concat to byte array buffers to one"() {
+        // TODO
+    }
+
+    def "calculateChunkOffsetUncompressed"() {
+        // TODO
+    }
+
+    def "calculateChunkOffsetCompressed"() {
+        // TODO
+    }
+
+    def "matchingFilter"() {
+        // TODO
+    }
+
+    def "getDataSetFromOffsetsGroup"() {
+        // TODO
+    }
+
+    def "findDatasetLengthByLineBreak"() {
+        // TODO
+    }
+
+    def "getBufferByOffsetGroup"() {
+        // TODO
+    }
+
+    def "groupOffsetsByBufferSize"() {
+        // TODO
+    }
 }

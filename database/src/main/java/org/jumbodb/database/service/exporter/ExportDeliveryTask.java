d168051737c87b546ab3aaa1c8e0379b1ef8b11e
Carsten Hufe <carsten.hufe@comsysto.com>
2013-09-06 09:53:59 +0200

    replication bugfix

--- database/src/main/java/org/jumbodb/database/service/exporter/ExportDeliveryTask.java
+++ database/src/main/java/org/jumbodb/database/service/exporter/ExportDeliveryTask.java
@@ -33,6 +33,9 @@ public class ExportDeliveryTask implements Runnable {
 
     @Override
     public void run() {
+        if(exportDelivery.getState() == ExportDelivery.State.ABORTED) {
+            return;
+        }
         JumboImportConnection imp = null;
         try {
             exportDelivery.setState(ExportDelivery.State.RUNNING);

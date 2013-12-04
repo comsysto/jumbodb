ef1ce9953fc026caab9ae872e07e6b46f4994053
Carsten Hufe <carsten.hufe@comsysto.com>
2013-12-04 18:24:44 +0100

    flush added again

--- connectors/jumbodb-java-connector/src/main/java/org/jumbodb/connector/importer/JumboImportConnection.java
+++ connectors/jumbodb-java-connector/src/main/java/org/jumbodb/connector/importer/JumboImportConnection.java
@@ -39,7 +39,7 @@ public class JumboImportConnection implements Closeable {
             sis = new SnappyInputStream(is);
             dis = new DataInputStream(sis);
             dos.writeInt(JumboConstants.IMPORT_PROTOCOL_VERSION);
-//            dos.flush();
+            dos.flush();
         } catch (IOException e) {
             throw new UnhandledException(e);
         }

d41048eea2d45b4edf52f4907d1ec502c09ba69b
Carsten Hufe <carsten.hufe@devproof.org>
2014-03-04 18:56:11 +0100

    checksum file suffix

--- commons/data/src/main/java/org/jumbodb/data/common/meta/ChecksumType.java
+++ commons/data/src/main/java/org/jumbodb/data/common/meta/ChecksumType.java
@@ -5,17 +5,23 @@ package org.jumbodb.data.common.meta;
  */
 public enum ChecksumType {
 
-    NONE("none"),
-    MD5("MD5"),
-    SHA1("SHA-1");
+    NONE("none", "none"),
+    MD5("MD5", ".md5"),
+    SHA1("SHA-1", ".sha1");
 
     String digest;
+    String fileSuffix;
 
-    ChecksumType(final String digest) {
+    ChecksumType(String digest, String fileSuffix) {
         this.digest = digest;
+        this.fileSuffix = fileSuffix;
     }
 
     public String getDigest() {
         return digest;
+    }
+
+    public String getFileSuffix() {
+        return fileSuffix;
     }
 }

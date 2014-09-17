8b8d2640580c42284674724c02d77ed8172f7615
Carsten Hufe <carsten.hufe@devproof.org>
2014-09-17 22:31:10 +0200

    first selective test

--- commons/query/src/main/java/org/jumbodb/common/query/QueryOperation.java
+++ commons/query/src/main/java/org/jumbodb/common/query/QueryOperation.java
@@ -1,6 +1,7 @@
 package org.jumbodb.common.query;
 
 public enum QueryOperation {
+    // CARSTEN fill with meta info to make error messages better ... GT(">") etc ...
     OR, EQ, NE, LT, GT, BETWEEN, GEO_BOUNDARY_BOX, GEO_WITHIN_RANGE_METER
     // CARSTEN implement these types:
     // CARSTEN LIKE, LT_EQ, GT_EQ, IS NULL, IS NOT NULL, EXISTS, NOT EXISTS, IN
